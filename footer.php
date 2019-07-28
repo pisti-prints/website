dbecdc778f08e639de42940bf21125d08713adcb
Anderson Koh <andersonkoh96@gmail.com>
2019-07-28 20:21:20 +0800

    removed assets folder and adjusted footer img link

--- footer.php
+++ footer.php
@@ -13,113 +13,129 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="" class="">
-		<div class="pisti-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-12 col-md-4 col-lg-3">
-						<p class="pisti-footer-title">Quick links</p>
-						<div class="pisti-footer-divider"></div>
-						<ul class="pisti-footer-ul pisti-quicklinks">
-							<li class="footerlink"><a href="/">Home</a></li>
-							<li class="footerlink"><a href="/products/">Products</a></li>
-							<li class="footerlink"><a href="/faq/">F.A.Q</a></li>
-							<li class="footerlink"><a href="/contact/">Contact us</a></li>
-							<li class="footerlink"><a href="/contact/">Request quotation</a></li>
+<footer class="">
+	<div class="pisti-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-4 col-lg-3">
+					<p class="pisti-footer-title">Quick links</p>
+					<div class="pisti-footer-divider"></div>
+					<ul class="pisti-footer-ul pisti-quicklinks">
+						<li class="footerlink"><a href="/">Home</a></li>
+						<li class="footerlink"><a href="/products/">Products</a></li>
+						<li class="footerlink"><a href="/faq/">F.A.Q</a></li>
+						<li class="footerlink"><a href="/contact/">Contact us</a></li>
+						<li class="footerlink"><a href="/contact/">Request quotation</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-md-4 col-lg-3">
+					<p class="pisti-footer-title pisti-footer-title-follow">Solutions for</p>
+					<div class="pisti-footer-divider"></div>
+					<ul class="pisti-footer-ul pisti-solutionsfor">
+						<li class="footerlink"><a href="/best-seller/">Best seller</a></li>
+						<li class="footerlink"><a href="/gift/">Business essentials</a></li>
+						<li class="footerlink"><a href="/large-format-printing/">Large format</a></li>
+						<li class="footerlink"><a href="/designing-services/">Design services</a></li>
+					</ul>
+				</div>
+				<div class="col-12 col-md-4 col-lg-3">
+					<p class="pisti-footer-title pisti-footer-title-follow">Pisti support</p>
+					<div class="pisti-footer-divider"></div>
+					<ul class="pisti-footer-ul pisti-pistisupport">
+						<li class="footerlink line"><a href="mailto:sales@pistiprints.com?cc=sales2@pistiprints.com?subject=Enquiries&body=Dear%20Pisti%20Prints,"
+								target="_blank">sales@pistiprints.com</a></li>
+						<li class="footerlink line"><a href="tel:+6566358358" target="_blank">Office: +65 6635 8358</a>
+						</li>
+						<li class="footerlink line"><a href="tel:+6596856011" target="_blank">Mobile: +65 9685 6011</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-12 col-md-12 col-lg-3 pisti-footer-title-follow pisti-footer-title-follow-alt">
+					<p class="pisti-footer-title">Business hours</p>
+					<div class="pisti-footer-divider"></div>
+					<ul class="pisti-footer-ul pisti-businesshours">
+						<li class="footerlink">Mon - Fri: 9:30 am - 6:30 pm</li>
+						<li class="footerlink">Sat - Pub: Closed</li>
+						<li class="footerlink"><a href="https://goo.gl/maps/B4p8zsjjy5AD661p9"
+								target="_blank">ARK@Gambas,<br>7 Gambas Cres, #05-18,<br>Singapore. (757087)</a></li>
+					</ul>
+				</div>
+			</div>
+			<hr class="pisti-footer-hr">
+			<div class="row">
+				<div class="col-12 col-md-8 col-lg-9">
+					<p class="pisti-footer-title">Others</p>
+					<div class="pisti-footer-divider"></div>
+					<div class="pisti-others d-lg-flex">
+						<ul class="pisti-footer-ul pisti-others mr-30px mr-m-0">
+							<li class="footerlink"><a href="/sitemap/">Sitemap</a></li>
+							<li class="footerlink"><a href="/privacy-policy/">Privacy policy</a></li>
+							<li class="footerlink"><a href="/career/">Career</a></li>
 						</ul>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3">
-						<p class="pisti-footer-title pisti-footer-title-follow">Solutions for</p>
-						<div class="pisti-footer-divider"></div>
-						<ul class="pisti-footer-ul pisti-solutionsfor">
-							<li class="footerlink"><a href="/best-seller/">Best seller</a></li>
-							<li class="footerlink"><a href="/gift/">Business essentials</a></li>
-							<li class="footerlink"><a href="/large-format-printing/">Large format</a></li>
-							<li class="footerlink"><a href="/designing-services/">Artwork design</a></li>
-						</ul>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3">
-						<p class="pisti-footer-title pisti-footer-title-follow">Pisti support</p>
-						<div class="pisti-footer-divider"></div>
-						<ul class="pisti-footer-ul pisti-pistisupport">
-							<li class="footerlink line"><a href="mailto:sales@pistiprints.com" target="_blank">sales@pistiprints.com</a></li>
-							<li class="footerlink line"><a href="tel:+6566358358" target="_blank">Office: +65 6635 8358</a></li>
-							<li class="footerlink line"><a href="tel:+6596856011" target="_blank">Mobile: +65 9685 6011</a></li>
-						</ul>
-					</div>
-					<div class="col-12 col-md-12 col-lg-3 pisti-footer-title-follow pisti-footer-title-follow-alt">
-						<p class="pisti-footer-title">Business hours</p>
-						<div class="pisti-footer-divider"></div>
-						<ul class="pisti-footer-ul pisti-businesshours">
-							<li class="footerlink">Mon - Fri: 9:30 am - 6:30 pm</li>
-							<li class="footerlink">Sat - Pub: Closed</li>
-							<li class="footerlink"><a href="https://goo.gl/maps/B4p8zsjjy5AD661p9" target="_blank">ARK@Gambas,<br>7 Gambas Cres, #05-18,<br>Singapore. (757087)</a></li>
+						<ul class="pisti-footer-ul pisti-others">
+							<li class="footerlink"><a href="/delivery-schedule/">Delivery schedule</a></li>
+							<li class="footerlink"><a href="/term-of-use/">Terms of use</a></li>
+							<li class="footerlink"><a href="/artwork-specification/">Artwork specification</a></li>
 						</ul>
 					</div>
 				</div>
-				<hr class="pisti-footer-hr">
-				<div class="row">
-					<div class="col-12 col-md-8 col-lg-9">
-						<p class="pisti-footer-title">Others</p>
-						<div class="pisti-footer-divider"></div>
-						<div class="pisti-others d-lg-flex">
-							<ul class="pisti-footer-ul pisti-others mr-30px mr-m-0">
-								<li class="footerlink"><a href="/sitemap/">Sitemap</a></li>
-								<li class="footerlink"><a href="/privacy-policy/">Privacy policy</a></li>
-								<li class="footerlink"><a href="/career/">Career</a></li>
-							</ul>
-							<ul class="pisti-footer-ul pisti-others">
-								<li class="footerlink"><a href="/delivery-schedule/">Delivery schedule</a></li>
-								<li class="footerlink"><a href="/term-of-use/">Terms of use</a></li>
-								<li class="footerlink"><a href="/artwork-specification/">Artwork specification</a></li>
-							</ul>
-						</div>
-						<!-- <div class="mb-4"></div>
-                    	<div class="pisti-others">
-							<ul class="pisti-footer-ul pisti-others mr-30px mr-m-0">
-                            	<li class="footerlink d-flex"><img width="30pt" style="margin-right: 10px;" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/iconmonstr-help-3.svg" /><div>Need help? Visit the <a href="/faq/" style="text-decoration: underline;">F.A.Q here</a></div></li>
-							</ul>
-						</div> -->
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 ml-auto">
-						<p class="pisti-footer-title pisti-footer-title-follow">Follow us</p>
-						<div class="pisti-footer-divider"></div>
-						<ul class="pisti-footer-ul pisti-socmed">
-							<li class="footerlink socmed socmed-ig"><a href="https://www.instagram.com/pistiprints/" target="_blank" class="" style="background-image: url('<?php bloginfo('stylesheet_directory'); ?>/assets/soc-med-icon.svg');"></a></li>
-							<li class="footerlink socmed socmed-fb"><a href="https://www.facebook.com/pistiprint/" target="_blank" class="" style="background-image: url('<?php bloginfo('stylesheet_directory'); ?>/assets/soc-med-icon.svg');"></a></li>
-							<li class="footerlink socmed socmed-twtr"><a href="https://twitter.com/pistiprints/" target="_blank" class="" style="background-image: url('<?php bloginfo('stylesheet_directory'); ?>/assets/soc-med-icon.svg');"></a></li>
+				<div class="col-12 col-md-4 col-lg-3 ml-auto">
+					<p class="pisti-footer-title pisti-footer-title-follow">Follow us</p>
+					<div class="pisti-footer-divider"></div>
+					<ul class="pisti-footer-ul pisti-socmed">
+						<li class="footerlink socmed socmed-ig"><a href="https://www.instagram.com/pistiprints/"
+								target="_blank" class=""
+								style="background-image: url('https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-soc-med-icon.svg');"></a>
+						</li>
+						<li class="footerlink socmed socmed-fb"><a href="https://www.facebook.com/pistiprint/"
+								target="_blank" class=""
+								style="background-image: url('https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-soc-med-icon.svg"></a>
+						</li>
+						<li class="footerlink socmed socmed-twtr"><a href="https://twitter.com/pistiprints/"
+								target="_blank" class=""
+								style="background-image: url('https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-soc-med-icon.svg"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="pisti-rights">
+		<div class="container">
+			<div class="row d-flex align-items-center">
+				<div class="col-12 col-md-6 mr-auto">
+					<p class="pisti-copyright">© 2013 - 2019 PISTIPRINTS. All rights reserved.</p>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="payment-gateway">
+						<ul>
+							<li class="gateway payment-1"><img width="60pt"
+									src="https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-bank-transfer.svg"
+									class="img-fluid" alt="pisti-payment-bank-transfer" /></li>
+							<li class="gateway payment-2"><img width="60pt"
+									src="https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-cash-dollar.svg"
+									class="img-fluid" alt="pisti-payment-cash-dollar" /></li>
+							<li class="gateway payment-3"><img width="60pt"
+									src="https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-dbs-paylah.svg"
+									class="img-fluid" alt="pisti-payment-paylah" /></li>
+							<li class="gateway payment-4"><img width="60pt"
+									src="https://asign.pro/wp-content/uploads/asset/icon/pistiprints-icon-paynow-color.svg"
+									class="img-fluid" alt="pisti-payment-paynow" /></li>
 						</ul>
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<div class="pisti-rights">
-			<div class="container">
-				<div class="row d-flex align-items-center">
-					<div class="col-12 col-md-6 mr-auto">
-						<p class="pisti-copyright">© 2013 - 2019 PISTIPRINTS. All rights reserved.</p>
-					</div>
-					<div class="col-12 col-md-6">
-						<div class="payment-gateway">
-							<ul>
-								<li class="gateway payment-1"><img width="60pt" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/bank-transfer.svg" class="img-fluid" alt="pisti-payment-bank-transfer"/></li>
-								<li class="gateway payment-2"><img width="60pt" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/cash-dollar.svg" class="img-fluid" alt="pisti-payment-cash-dollar"/></li>
-								<li class="gateway payment-3"><img width="60pt" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dbs-paylah.svg" class="img-fluid" alt="pisti-payment-paylah"/></li>
-								<li class="gateway payment-4"><img width="60pt" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/paynow-color.svg" class="img-fluid" alt="pisti-payment-paynow"/></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer><!-- #colophon -->
+	</div>
+</footer><!-- #colophon -->
 
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
